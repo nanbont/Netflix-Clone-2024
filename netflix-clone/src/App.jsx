322e4ca50de9fefac6c21677385e852186d6a110
nanbont <156798842+nanbont@users.noreply.github.com>
2024-05-03 21:32:18 +0300

    first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+      <div>
+        <h1>Netflix-clone-2024</h1>
+      </div>
+       
+  )
+}
+
+export default App
