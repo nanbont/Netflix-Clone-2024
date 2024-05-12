390df7490f30508cd7fd2a48f2d6c5dae48fd226
nanbont <156798842+nanbont@users.noreply.github.com>
2024-05-13 02:54:39 +0300

    I added the Arrow Icons to it

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Pages/Home/Home'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
--- netflix-clone/src/components/Rows/Row/Row.jsx
+++ netflix-clone/src/components/Rows/Row/Row.jsx
@@ -1,44 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import "./Row.css"
+import React, { useEffect, useState, useRef } from 'react';
+import "./Row.css";
 import axios from "../../../utils/axios";
 import movieTrailer from 'movie-trailer';
 import YouTube from 'react-youtube';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovie] = useState([]);
     const [trailerUrl, setTrailerUrl] = useState("");
+    const rowRef = useRef(null);
 
     const base_url = "https://image.tmdb.org/t/p/original";
 
-
     useEffect(() => {
-        (async () => {
+        async function fetchData() {
             try {
-                // console.log(fetchUrl)
-                // const request = await axios.get(http://localhost:3000/api/${fetchUrl});
                 const request = await axios.get(fetchUrl);
-                console.log(request)
                 setMovie(request.data.results);
             } catch (error) {
-                console.log("error", error);
+                console.error("Error fetching data:", error);
             }
-        })()
+        }
+        fetchData();
     }, [fetchUrl]);
 
     const handleClick = (movie) => {
         if (trailerUrl) {
-            setTrailerUrl('')
+            setTrailerUrl('');
         } else {
-            movieTrailer(movie?.title + " " + movie?.name + " " + movie?.original_name)
-            .then((url) => {
-                    // console.log(url)
-                    const urlParams = new URLSearchParams(new URL(url).search)
-                    // console.log(urlParams)
-                    // console.log(urlParams.get('v'))
+            movieTrailer(movie?.title || movie?.name || movie?.original_name)
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
                     setTrailerUrl(urlParams.get('v'));
                 })
+                .catch((error) => console.error(error));
         }
-    }
+    };
 
     const opts = {
         height: '390',
@@ -46,30 +44,44 @@ function Row({ title, fetchUrl, isLargeRow }) {
         playerVars: {
             autoplay: 1,
         },
-    }
+    };
 
-return (
-    <div className="row">
-        <h1>{title}</h1>
-        <div className="row__posters">
-            {movies?.map((movie, index) => (
-                <div key={index} className="row__posterContainer">
-                    <img
-  onClick={() => handleClick(movie)}
-  src={base_url + (isLargeRow ? movie.poster_path : movie.backdrop_path)}
-  alt={movie.name}
-/>
+    const handleScroll = (direction) => {
+        if (rowRef.current) {
+            const { scrollLeft, clientWidth } = rowRef.current;
+            const scrollTo = direction === 'left' ? scrollLeft - clientWidth : scrollLeft + clientWidth;
+            rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' });
+        }
+    };
 
-<div className="row__posterTitle">{movie?.title || movie?.name || movie?.original_name}</div>
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <button className="row__arrow row__arrowLeft" onClick={() => handleScroll('left')}>
+                <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+            <div className="row__posters" ref={rowRef}>
+    {movies.map((movie, index) => (
+        <div key={index} className="row__posterContainer">
+            <img
+                className={`row__poster ${isLargeRow ? "row__posterLarge" : ""}`}
+                onClick={() => handleClick(movie)}
+                src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                alt={movie.name}
+            />
+            <div className="row__posterTitle">{movie.name}</div>
+        </div>
+    ))}
 </div>
-            ))}
+
+            <button className="row__arrow row__arrowRight" onClick={() => handleScroll('right')}>
+                <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+            {trailerUrl && (
+                <YouTube videoId={trailerUrl} opts={opts} />
+            )}
         </div>
-        
-        <div style={{ padding: '30px' }}>
-            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-        </div>
-    </div>
-)
+    );
 }
 
-export default Row
+export default Row;
