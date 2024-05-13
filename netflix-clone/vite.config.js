078d0320bc23065624b0c9ed9d9a9bf0d6286cfe
nanbont <156798842+nanbont@users.noreply.github.com>
2024-05-13 16:30:47 +0300

    now

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2024/netflix-clone/",
+  base: "https://nanbont.github.io/Netflix-Clone-2024/netflix-clone/",
 })
 
 
